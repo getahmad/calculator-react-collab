15de3853641f23bb41b14692efed8e662147853f
Ahmad Danang <ahmad.danang91@gmail.com>
2020-10-30 14:06:54 +0700

    calcu init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Calculator from "./components/Calculator"
+import "./css/style.css"
+// import Display from "./components/Display"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+   render (){
+     return(
+       <div>
+         <h1>Calculator</h1>
+         <Calculator/>
+       </div>
+
+     )
+  }
+
 }
 
 export default App;
+
