548091629ac46b7a5058b72fb5b899359754a69a
aridarsan <aridarsan09@gmail.com>
2020-10-31 17:00:54 +0700

    update style

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import Calculator from "./components/Calculator"
-import "./css/style.css"
+import "./css/style.css";
 // import Display from "./components/Display"
 
 class App extends Component {
 
    render (){
      return(
-       <div>
+       <div className="calculator-app">
          <h1>Calculator</h1>
          <Calculator/>
+         <h4>This App Made With Pride By :</h4>
+         <h3>Ahmad Danang X Ari Darsan X Muhammad Arief</h3>
        </div>
 
      )
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -19,47 +19,51 @@ class Calculator extends Component {
     });
   }
 
-  Calculate() {
+  Calculate(digit) {
     const { count } = this.state;
     this.setState({
+      // eslint-disable-next-line no-eval
       count: eval(count),
     });
   }
 
   render() {
     return (
-      <div className="container">
-       <div className="calculator">
-       <div className="row">
-          <input value={this.state.count} />
-        </div>
-        <div className="row">
-          <button onClick={() => this.changeValue(7)}>7</button>
-          <button onClick={() => this.changeValue(8)}>8</button>
-          <button onClick={() => this.changeValue(9)}>9</button>
-          <button onClick={() => this.changeValue("+")}  className="operator">+</button>
-        </div>
-        <div className="row">
-          <button onClick={() => this.changeValue(4)}>4</button>
-          <button onClick={() => this.changeValue(5)}>5</button>
-          <button onClick={() => this.changeValue(6)}>6</button>
-          <button onClick={() => this.changeValue("-")} className="operator">-</button>
-        </div>
-        <div className="row">
-          <button onClick={() => this.changeValue(1)}>1</button>
-          <button onClick={() => this.changeValue(2)}>2</button>
-          <button onClick={() => this.changeValue(3)}>3</button>
-          <button onClick={() => this.changeValue("*")} className="operator"> *</button>
-        </div>
-        <div className="row">
-          <button onClick={() => this.Reset()} className="operator">C</button>
-          <button onClick={() => this.changeValue(0)}>0</button>
-          <button onClick={() => this.changeValue("/")} className="operator">/</button>
-          <button className="calculate operator" onClick={() => this.Calculate()}>
-            =
-          </button>
-        </div>
-
+      <div className="container-calc">
+        <div className="calculator">
+          <div className="display">
+            <div className="row">
+            < input value={this.state.count} />
+            </div>
+          </div>
+          <div className="keyboard">
+            <div className="row">
+              <button onClick={() => this.changeValue(7)}>7</button>
+              <button onClick={() => this.changeValue(8)}>8</button>
+              <button onClick={() => this.changeValue(9)}>9</button>
+              <button onClick={() => this.changeValue("+")}  className="operator">+</button>
+            </div>
+            <div className="row">
+              <button onClick={() => this.changeValue(4)}>4</button>
+              <button onClick={() => this.changeValue(5)}>5</button>
+              <button onClick={() => this.changeValue(6)}>6</button>
+              <button onClick={() => this.changeValue("-")} className="operator">-</button>
+            </div>
+            <div className="row">
+              <button onClick={() => this.changeValue(1)}>1</button>
+              <button onClick={() => this.changeValue(2)}>2</button>
+              <button onClick={() => this.changeValue(3)}>3</button>
+              <button onClick={() => this.changeValue("*")} className="operator"> *</button>
+            </div>
+            <div className="row">
+              <button onClick={() => this.Reset()} className="operator">C</button>
+              <button onClick={() => this.changeValue(0)}>0</button>
+              <button onClick={() => this.changeValue("/")} className="operator">/</button>
+              <button className="calculate operator" onClick={() => this.Calculate()}>
+                =
+              </button>
+            </div>
+          </div>
        </div>
       </div>
     );
